870faadf76b0d047decb6996b9328aeee3ed5c20
HYMONNET <David>
2014-06-23 16:19:12 +0200

    Add Login Form

--- yanos/accounts/forms.py
+++ yanos/accounts/forms.py
@@ -6,3 +6,7 @@ class RegisterForm(Form):
 	username = TextField('Login', validators=[Required()])
 	password = PasswordField('Password', validators=[Required()])
 	password2 = PasswordField('Password2', validators=[Required()])
+
+class LoginForm(Form):
+	username = TextField('Login', validators=[Required()])
+	password = PasswordField('Password', validators=[Required()])
