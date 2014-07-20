6d8997a9ee37966fa93cf8577574c3d839cc734b
bougie <bougie@appartland.eu>
2014-07-20 20:52:20 +0200

    test(tasks): Write unit tests for priority module

--- tasks/tests.py
+++ tasks/tests.py
@@ -1,3 +1,53 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Priority
+from lib.exception import CoreException
+from .core.priority import corePriorityAdd, corePriorityDelete, \
+    corePriorityEdit, corePriorityList
+
+
+class PriorityTestCase(TestCase):
+    def setUp(self):
+        Priority(name='existing_priority').save()
+
+    def test_creating_success(self):
+        corePriorityAdd('test')
+        self.assertEqual(2, Priority.objects.all().count())
+
+    def test_creating_existing_name(self):
+        try:
+            corePriorityAdd('existing_priority')
+        except CoreException as e:
+            self.assertEqual(1, e.code)
+
+    def test_deleting_success(self):
+        corePriorityDelete(1)
+
+    def test_deleting_non_existing(self):
+        try:
+            corePriorityDelete(1337)
+        except CoreException as e:
+            self.assertEqual(0, e.code)
+
+    def test_editing_success(self):
+        corePriorityEdit(1, 'newname')
+
+        pri = Priority.objects.get(name='newname')
+        self.assertIsNotNone(pri)
+
+    def test_editing_non_existing(self):
+        try:
+            corePriorityEdit(1337, 'fubarland')
+        except CoreException as e:
+            self.assertEqual(0, e.code)
+
+    def test_editing_existing_name(self):
+        try:
+            corePriorityEdit(1, 'existing_priority')
+        except CoreException as e:
+            self.assertEqual(1, e.code)
+
+    def test_listing(self):
+        priorities = corePriorityList()
+        self.assertEqual(1, len(priorities))
+        self.assertEqual(priorities[0]['name'], 'existing_priority')
