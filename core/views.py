fbbdcc0abc004438e51a0084bb98d93a687fbfc0
bougie <bougie@appartland.eu>
2014-07-20 14:25:20 +0200

    fix(core): Bad char in title
    Forgetting to replace html entitie with the real character in the page
    title

--- core/views.py
+++ core/views.py
@@ -3,6 +3,6 @@ from lib.renderer import request_render
 
 def index(request):
     tpl_vars = {
-        'page_title': '\_o&lt;~ KOIN KOIN INDEX'
+        'page_title': '\_o<~ KOIN KOIN INDEX'
     }
     return request_render(request, 'index.j2', tpl_vars)
